d44158e331daaea8433c882bc339dc980117e81c
Tom Zhang <tzhang@evernote.com>
2016-01-11 14:16:41 -0800

    Changed port to use the server's port or 3000

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// server.js
+/*
+ * Evernote-webhooks: A project to use webhooks to automate things in Evernote
+ */
 // a very simple node server using the express module
 var express = require("express");
 var app = express();
 var wwwDir = "/www";
 app.use("/", express.static(__dirname + wwwDir));
 app.get("/", function(req, res) { res.render(wwwDir + "/index.html");});
-app.listen(8080);
+// Start the server on port 3000 or the server port.
+var port = process.env.PORT || 3000;
+console.log("PORT: " + port);
+var server = app.listen(port);
