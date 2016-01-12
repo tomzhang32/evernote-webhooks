1d6d413d9ae3a7b8af52eb58c2343a0513458a79
Tom Zhang <tzhang@evernote.com>
2016-01-11 18:05:11 -0800

    Added a link to /OAuth in index.html and log the endpoint hit server-side

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 /*
  * Evernote-webhooks: A project to use webhooks to automate things in Evernote
  */
-// a very simple node server using the express module
-var express = require("express");
+var express = require('express');
+var config = require('./config.json');
+
 var app = express();
 var consumerKey = process.env.consumerKey;
 var consumerSecret = process.env.consumerSecret;
 var wwwDir = "/www";
-app.use("/", express.static(__dirname + wwwDir));
-app.get("/", function(req, res) { res.send("Consumer key: " + consumerKey);});
+
+app.use('/', express.static(__dirname + wwwDir));
+app.get('/:endpoint', function(req, res, next) { console.log(req.params.endpoint); next(); });
+app.get('/', function(req, res) { res.render(wwwDir + '/index.html');});
+
 // Start the server on port 3000 or the server port.
 var port = process.env.PORT || 3000;
-console.log("PORT: " + port);
+console.log('PORT: ' + port);
 var server = app.listen(port);
