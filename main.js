43ab7abfed2ec2c4768fa2fb03885ecb64c48f26
Tom Zhang <tzhang@evernote.com>
2016-01-12 15:02:21 -0800

    Configured express-session and set up an OAuth callback
    Saving the OAuth token to the request session.
    Need to do more digging as to how the callback works.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var Evernote = require('evernote').Evernote;
-var session = require('express-session');
+var expressSession = require('express-session');
 var config;
 try {
   config = require('./config.json');
@@ -18,28 +18,67 @@ try {
 var app = express();
 var wwwDir = "/www";
 
-// app.use(express.session());
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'this is a secret'
+}));
 app.use('/', express.static(__dirname + wwwDir));
+
 app.get('/OAuth', function(req, res) {
   var client = new Evernote.Client({
     consumerKey: config.API_CONSUMER_KEY,
     consumerSecret: config.API_CONSUMER_SECRET,
     sandbox: config.SANDBOX
   });
+  console.log(req.session);
 
-  client.getRequestToken('/', function(error, oauthToken, oauthTokenSecret, results){
+  client.getRequestToken('/OAuthCallback', function(error, oauthToken, oauthTokenSecret, results){
     if (error) {
       console.log(JSON.stringify(error));
       res.send(error.data);
     } else { 
       // store the tokens in the session
-      session.oauthToken = oauthToken;
-      session.oauthTokenSecret = oauthTokenSecret;
+      req.session.hasRequestToken = true;
+      req.session.oauthToken = oauthToken;
+      req.session.oauthTokenSecret = oauthTokenSecret;
 
       // redirect the user to authorize the token
       res.redirect(client.getAuthorizeUrl(oauthToken));
     }
   });
+});
+
+app.get('/OAuthCallback', function(req, res) {
+  var client = new Evernote.Client({
+    consumerKey: config.API_CONSUMER_KEY,
+    consumerSecret: config.API_CONSUMER_SECRET,
+    sandbox: config.SANDBOX
+  });
+  console.log(req.session);
+
+  client.getAccessToken(
+    req.session.oauthToken,
+    req.session.oauthTokenSecret,
+    req.param('oauth_verifier'),
+    function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+      if(error) {
+        console.log('error');
+        console.log(error);
+        res.redirect('/');
+      } else {
+        // store the access token in the session
+        req.session.oauthAccessToken = oauthAccessToken;
+        req.session.oauthAccessTtokenSecret = oauthAccessTokenSecret;
+        req.session.edamShard = results.edam_shard;
+        req.session.edamUserId = results.edam_userId;
+        req.session.edamExpires = results.edam_expires;
+        req.session.edamNoteStoreUrl = results.edam_noteStoreUrl;
+        req.session.edamWebApiUrlPrefix = results.edam_webApiUrlPrefix;
+        res.redirect('/');
+      }
+    }
+  );
 });
 
 app.get('/', function(req, res) { res.render(wwwDir + '/index.html');});
