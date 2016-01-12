e43d058dd3644fbfd7328616e176c1c2a775f676
Adam Krzesinski <akrzesinski@evernote.com>
2016-01-11 17:27:51 -0800

    Get api key/secret from the server env.

--- main.js
+++ main.js
@@ -5,9 +5,12 @@
 var express = require("express");
 var app = express();
 var wwwDir = "/www";
-app.use("/", express.static(__dirname + wwwDir));
-app.get("/", function(req, res) { res.render(wwwDir + "/index.html");});
-// Start the server on port 3000 or the server port.
 var port = process.env.PORT || 3000;
 console.log("PORT: " + port);
 var server = app.listen(port);
+var consumerKey = process.env.consumerKey;
+var consumerSecret = process.env.consumerSecret;
+
+app.use("/", express.static(__dirname + wwwDir));
+app.get("/", function(req, res) { res.render(wwwDir + "/index.html?consumerKey" + consumerKey);});
+// Start the server on port 3000 or the server port.
