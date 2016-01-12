22025d748f59e743ab91ad8bfadc42018e374579
Tom Zhang <tzhang@evernote.com>
2016-01-11 18:38:05 -0800

    Fixed express endpoints, add test for retrieving env vars

--- main.js
+++ main.js
@@ -9,7 +9,15 @@ var consumerSecret = process.env.consumerSecret;
 var wwwDir = "/www";
 
 app.use('/', express.static(__dirname + wwwDir));
-app.get('/:endpoint', function(req, res, next) { console.log(req.params.endpoint); next(); });
+app.get('/:endpoint', function(req, res, next) {
+  console.log(req.params.endpoint);
+  if (req.params.endpoint === 'consumerKey') {
+    res.send(consumerKey);
+  } else {
+    res.send(req.params.endpoint + ' endpoint reached');
+  }
+  next();
+});
 app.get('/', function(req, res) { res.render(wwwDir + '/index.html');});
 
 // Start the server on port 3000 or the server port.
