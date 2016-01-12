94aa554eeda39ca8f412042de4c4caba3d6307f0
Tom Zhang <tzhang@evernote.com>
2016-01-11 19:16:46 -0800

    Try to get OAuth working; stuck on the request token
    Set up the config variables to be loaded from config.json if it exists,
    or environment variables if not.
    
    Set up an OAuth request when the user hits the /OAuth page, that
    should redirect to Evernote.Client.getAuthorizeUrl on success.
    Currently, we are getting a 401 error.

--- main.js
+++ main.js
@@ -2,22 +2,45 @@
  * Evernote-webhooks: A project to use webhooks to automate things in Evernote
  */
 var express = require('express');
+var Evernote = require('evernote').Evernote;
+var config;
+try {
+  config = require('./config.json');
+} catch (e) {
+  config = {
+    API_CONSUMER_KEY: process.env.consumerKey,
+    API_CONSUMER_SECRET: process.env.consumerSecret,
+    SANDBOX: true
+  };
+}
 
 var app = express();
-var consumerKey = process.env.consumerKey;
-var consumerSecret = process.env.consumerSecret;
 var wwwDir = "/www";
 
+// app.use(express.session());
 app.use('/', express.static(__dirname + wwwDir));
-app.get('/:endpoint', function(req, res, next) {
-  console.log(req.params.endpoint);
-  if (req.params.endpoint === 'consumerKey') {
-    res.send(consumerKey);
-  } else {
-    res.send(req.params.endpoint + ' endpoint reached');
-  }
-  next();
+app.get('/OAuth', function(req, res) {
+  var client = new Evernote.Client({
+    consumerKey: config.API_CONSUMER_KEY,
+    consumerSecret: config.API_CONSUMER_SECRET,
+    sandbox: config.SANDBOX
+  });
+
+  client.getRequestToken('/foo', function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      console.log(JSON.stringify(error));
+      res.send(error.data);
+    } else { 
+      // store the tokens in the session
+      req.session.oauthToken = oauthToken;
+      req.session.oauthTokenSecret = oauthTokenSecret;
+
+      // redirect the user to authorize the token
+      res.redirect(client.getAuthorizeUrl(oauthToken));
+    }
+  });
 });
+
 app.get('/', function(req, res) { res.render(wwwDir + '/index.html');});
 
 // Start the server on port 3000 or the server port.
