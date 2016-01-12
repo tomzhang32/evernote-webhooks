eb08af014766dc6fee998993f8e9c9cc2cb0fee5
Adam Krzesinski <akrzesinski@evernote.com>
2016-01-11 17:37:42 -0800

    Changing to app.respond

--- main.js
+++ main.js
@@ -4,13 +4,12 @@
 // a very simple node server using the express module
 var express = require("express");
 var app = express();
+var consumerKey = process.env.consumerKey;
+var consumerSecret = process.env.consumerSecret;
 var wwwDir = "/www";
+app.use("/", express.static(__dirname + wwwDir));
+app.get("/", function(req, res) { res.respond(wwwDir + "/index.html?consumerKey=" + consumerKey);});
+// Start the server on port 3000 or the server port.
 var port = process.env.PORT || 3000;
 console.log("PORT: " + port);
 var server = app.listen(port);
-var consumerKey = process.env.consumerKey;
-var consumerSecret = process.env.consumerSecret;
-
-app.use("/", express.static(__dirname + wwwDir));
-app.get("/", function(req, res) { res.render(wwwDir + "/index.html?consumerKey" + consumerKey);});
-// Start the server on port 3000 or the server port.
