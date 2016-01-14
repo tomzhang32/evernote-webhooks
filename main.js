519ac42aaaf24da647c4caa54ef4a961cbe430df
Tom Zhang <tzhang@evernote.com>
2016-01-13 16:32:36 -0800

    Added API calls to list notes and more comments
    Added an endpoint, /listNotes, to log notes in an account
    Log the history of the queries at /history
    Added links to make it easier to travel around the site
    Add comments to endpoints explaining roughly what they are

--- main.js
+++ main.js
@@ -21,6 +21,13 @@ config.SERVICE_BASE = config.SERVICE_BASE || process.env.serviceBase;
 var oauthCallbackUrl = config.SERVICE_BASE + '/OAuthCallback';
 
 var usersMap = new UsersMap();
+var history = [];
+var resultSpec = new Evernote.NotesMetadataResultSpec({
+  includeTitle: true,
+  includeNotebookGuid: true,
+  includeDeleted: true,
+  includeAttributes: true
+});
 
 var app = express();
 var wwwDir = '/www';
@@ -40,12 +47,88 @@ app.get('/error', function(req, res) {
   res.send('An error occurred. <a href="http://www.sadtrombone.com/?autoplay=true">Womp womp</a>');
 });
 
+/**
+ * The hook to receive Evernote events:
+ * https://dev.evernote.com/doc/articles/polling_notification.php
+ * Logs the query to the history array
+ * For our purposes, we only care about responding to note update events.
+ */
 app.get('/hook', function(req, res) {
   console.log('/hook');
-  console.log(req.query);
+  history.push(req.query);
   res.send(req.query);
 });
 
+/**
+ * Lists all queries that we've captured in our history
+ */
+app.get('/history', function(req, res) {
+  console.log('/history');
+  var response = '';
+  for (var i in history) {
+    response += 'request ' + i + ': '
+    response += JSON.stringify(history[i]);
+    response += '<br>'
+  }
+  res.send(response);
+});
+
+/**
+ * Logs some notes in case we want to inspect the details
+ */
+app.get('/listNotes', function(req, res) {
+  console.log('/listNotes');
+  if (req.query && req.query.userId) {
+    var userInfo = usersMap.getInfoForUser(req.query.userId);
+    if (userInfo) {
+      var noteFilter = new Evernote.NoteFilter({
+        inactive: false,
+        ascending: false, // newest first
+        words: ''
+      });
+
+      var client = new Evernote.Client({
+        token: userInfo.oauthAccessToken,
+        sandbox: config.SANDBOX
+      });
+      var noteStoreClient = client.getNoteStore();
+      noteStoreClient.findNotesMetadata(userInfo.oauthAccessToken, noteFilter, 0, 100, resultSpec,
+        function(err, noteList) {
+          if (err) {
+            console.log(err);
+            res.send(JSON.stringify(err));
+            return;
+          }
+          console.log(noteList.notes);
+
+          if (noteList.notes.length) {
+            response = '';
+            response += noteList.notes.length + ' notes found<br>';
+            response += 'first note:<br>';
+            response += 'guid: ' + noteList.notes[0].guid + '<br>';
+            response += 'title: ' + noteList.notes[0].title + '<br>';
+            response += 'notebookGuid: ' + noteList.notes[0].notebookGuid + '<br>';
+            response += 'tagGuids: ' + noteList.notes[0].tagGuids + '<br>';
+            response += '<a href="/hook?userId=' + req.query.userId + '&guid='
+              + noteList.notes[0].guid + '&notebookGuid=' + noteList.notes[0].notebookGuid
+              + '&reason=update">"update" this note</a>';
+
+            res.send(response);
+          } else {
+            res.send('No notes found!');
+          }
+        });
+
+      return;
+    }
+  }
+  res.send('User not found. <a href="/OAuth">Try OAuth</a>');
+});
+
+/**
+ * Begins the OAuth process. Based on
+ * https://github.com/evernote/evernote-sdk-js/blob/master/sample/express/routes/index.js
+ */
 app.get('/OAuth', function(req, res) {
   console.log('/OAuth');
   var client = new Evernote.Client({
@@ -71,6 +154,9 @@ app.get('/OAuth', function(req, res) {
   );
 });
 
+/**
+ * Once we have an OAuth request token, we
+ */
 app.get('/OAuthCallback', function(req, res) {
   console.log('/OAuthCallback');
   var client = new Evernote.Client({
@@ -98,6 +184,7 @@ app.get('/OAuthCallback', function(req, res) {
           results.edam_webApiUrlPrefix);
 
         req.session.oauthDone = true;
+        req.session.userId = results.edam_userId;
         res.redirect('/OAuthDone');
       }
     }
@@ -107,7 +194,7 @@ app.get('/OAuthCallback', function(req, res) {
 app.get('/OAuthDone', function(req, res) {
   console.log('/OAuthDone: ' + req.session.oauthDone);
   if (req.session.oauthDone) {
-    res.send('OAuth complete!');
+    res.send('OAuth complete! <a href="/listNotes?userId=' + req.session.userId + '">List notes</a>');
   } else {
     res.send('Error with OAuth; check the logs.');
   }
